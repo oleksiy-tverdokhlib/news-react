c4fc6f2dc83416fb9efb4feaad745aa5cb2064bd
Alex <oleksiytverd@gmail.com>
2025-05-16 17:44:27 +0300

    add news page and react router

--- src/entities/news/index.ts
+++ src/entities/news/index.ts
@@ -1,6 +1,7 @@
 import { INews, NewsApiResponse } from './model/types'
 import NewsCard from './ui/NewsCard/NewsCard'
+import NewsDetails from './ui/NewsDetails/NewsDetails';
 
-export { NewsCard }
+export { NewsCard, NewsDetails }
 export type { INews, NewsApiResponse }
 
